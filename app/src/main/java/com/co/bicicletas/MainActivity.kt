b5043ad5ebf0bfbd9438324e1c01d8dace2ec744
Juan Pablo rendon <juan.pablo.rendon@accenture.com>
2021-05-25 17:08:15 -0500

    Cambios para la creacion del login y carga de imagen y mensaje del boton

--- app/src/main/java/com/co/bicicletas/MainActivity.kt
+++ app/src/main/java/com/co/bicicletas/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.co.bicicletas
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.data
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var editTextHello:EditText
+    lateinit var editTextPass:EditText
+    lateinit var buttonAcc:Button
+    lateinit var linkOlvido: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editTextHello = findViewById(R.id.editText) as EditText
+        editTextPass = findViewById(R.id.editText2) as EditText
+        buttonAcc = findViewById(R.id.buttonIng) as Button
+        linkOlvido = findViewById(R.id.textOlvido) as TextView
+
+        buttonAcc.setOnClickListener {
+            Toast.makeText(
+                this, "Mensaje Boton" as String?,
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
+
+
 }
