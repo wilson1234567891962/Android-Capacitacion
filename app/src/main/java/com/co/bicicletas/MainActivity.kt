99bb8b52e412efbe196ff1504b21776b02560290
lucho <lucho2410_@hotmail.com>
2021-05-24 19:35:26 -0500

    se correlaciona la vista con el evento

--- app/src/main/java/com/co/bicicletas/MainActivity.kt
+++ app/src/main/java/com/co/bicicletas/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.co.bicicletas
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.data
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var usuario:EditText
+    lateinit var contraseña:EditText
+    lateinit var olvidoC:TextView
+    lateinit var buttonAcc: Button
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        usuario = findViewById(R.id.editText2) as EditText
+        contraseña = findViewById(R.id.editText) as EditText
+        olvidoC = findViewById(R.id.textView3) as TextView
+        buttonAcc = findViewById(R.id.button1) as Button
+        buttonAcc.setOnClickListener {
+            Toast.makeText(
+                this, "data.result" as String?,
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
+
+
+
+
+
 }
