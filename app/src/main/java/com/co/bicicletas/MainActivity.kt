1014e0cf2ae5ca33c8c27997ce693cf6b19d5550
xiomaraCortes <55770691+xiomaraCortes@users.noreply.github.com>
2021-05-24 23:50:10 -0500

    variables

--- app/src/main/java/com/co/bicicletas/MainActivity.kt
+++ app/src/main/java/com/co/bicicletas/MainActivity.kt
@@ -2,10 +2,23 @@ package com.co.bicicletas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var strUsuario : EditText
+    lateinit var strPass : EditText
+    lateinit var btnIngresar : Button
+    lateinit var TvOlvido : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        strUsuario = findViewById(R.id.txtUser) as EditText
+        strPass = findViewById(R.id.txtPass) as EditText
+        btnIngresar = findViewById(R.id.btIngresar) as Button
+        TvOlvido = findViewById(R.id.lblolvido) as TextView
+
     }
 }
