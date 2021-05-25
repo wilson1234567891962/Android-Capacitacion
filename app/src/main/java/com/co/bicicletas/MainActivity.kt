cf5ec48411527bde423784533b90ba49a0eddc04
sulanita <suanpa@gmail.com>
2021-05-24 19:53:05 -0500

    button click event

--- app/src/main/java/com/co/bicicletas/MainActivity.kt
+++ app/src/main/java/com/co/bicicletas/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.co.bicicletas
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity() : AppCompatActivity() {
+    lateinit var editTextHello:EditText
+    lateinit var editTextPass:EditText
+    lateinit var buttonIngresar:Button
+    lateinit var TextForget:TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        var editTextHello = findViewById(R.id.user) as EditText
+        var editTextPass = findViewById(R.id.password) as EditText
+        var buttonIngresar = findViewById(R.id.ingresar) as Button
+        var TextForget= findViewById(R.id.ingresar) as TextView
+
+
+        buttonIngresar.setOnClickListener {
+            Toast.makeText(
+                this, "Click!" as String?,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+
 }
+
+}
+
