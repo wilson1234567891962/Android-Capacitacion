cdedd68d5c218a31457a3661ca4be55f01d073fd
andurmon <andurmon@gmail.com>
2021-05-24 22:12:49 -0500

    imagen

--- app/src/main/java/com/co/bicicletas/MainActivity.kt
+++ app/src/main/java/com/co/bicicletas/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.co.bicicletas
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.data
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var editTextUsername : EditText
+    lateinit var editTextPassword: EditText
+    lateinit var submitBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+        editTextUsername = findViewById(R.id.username) as EditText
+        editTextPassword = findViewById(R.id.password) as EditText
+        submitBtn = findViewById(R.id.enviar) as Button
+
+        submitBtn.setOnClickListener {
+            Toast.makeText(
+                this, "data.result" as String?,
+                Toast.LENGTH_LONG
+            ).show()
+
+        }
 }
