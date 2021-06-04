50c63beccaa820b8b997dd80093ee7c7294c7bbf
Andres Zamora <andreszamora1998@gmailcom>
2021-06-04 18:21:33 -0500

    cards

---
+++ app/src/main/java/com/co/bicicletas/view/adapter/AlbumAdapter.kt
@@ -0,0 +1,59 @@
+package com.co.bicicletas.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.co.bicicletas.model.entities.Album
+import com.co.bicicletas.view.activities.databinding.ItemAlbumLayoutBinding
+
+
+class AlbumAdapter(private val fragment: Fragment) : RecyclerView.Adapter<AlbumAdapter.ViewHolder>() {
+    private var album: List<Album> = listOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ItemAlbumLayoutBinding =
+            ItemAlbumLayoutBinding.inflate(LayoutInflater.from(fragment.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    /**
+     * Binds each item in the ArrayList to a view
+     *
+     * Called when RecyclerView needs a new {@link ViewHolder} of the given type to represent
+     * an item.
+     *
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val dto = album[position]
+        // Load the dish image in the ImageView.
+        Glide.with(fragment)
+            .load(dto.cover)
+            .into(holder.ivDishImage)
+        holder.tvTitle.text = dto.genre
+    }
+    /**
+     * Gets the number of items in the list
+     */
+    override fun getItemCount(): Int {
+        return album.size
+    }
+    fun albumList(list: List<Album>) {
+        album = list
+        notifyDataSetChanged()
+    }
+    /**
+     * A ViewHolder describes an item view and metadata about its place within the RecyclerView.
+     */
+
+    class ViewHolder(view: ItemAlbumLayoutBinding) : RecyclerView.ViewHolder(view.root) {
+        // Holds the TextView that will add each item to
+        val ivDishImage = view.ivDishImage
+        val tvTitle = view.tvDishTitle
+        val ibMore = view.ibMore
+    }
+}
--- app/src/main/java/com/co/bicicletas/view/fragments/fragment_album.kt
+++ app/src/main/java/com/co/bicicletas/view/fragments/fragment_album.kt
@@ -1,4 +1,48 @@
 package com.co.bicicletas.view.fragments
 
-class fragment_album {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.co.bicicletas.model.entities.Album
+import com.co.bicicletas.view.activities.databinding.FragmentAlbumBinding
+import com.co.bicicletas.view.adapter.AlbumAdapter
+import com.co.bicicletas.viewModel.AlbumViewModel
+
+class fragment_album:Fragment() {
+    private lateinit var homeViewModel: AlbumViewModel
+    private var mBinding: FragmentAlbumBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel = ViewModelProvider(this).get(AlbumViewModel::class.java)
+        mBinding = FragmentAlbumBinding.inflate(inflater, container, false)
+        return mBinding!!.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Set the LayoutManager that this RecyclerView will use.
+        mBinding!!.rvAlbumList.layoutManager =
+            GridLayoutManager(requireActivity(), 2)
+        // Adapter class is initialized and list is passed in the param.
+        val adapter = AlbumAdapter(this@fragment_album)
+        // adapter instance is set to the recyclerview to inflate the items.
+        mBinding!!.rvAlbumList.adapter = adapter
+        var albums = listOf<Album>(Album("https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg","Rock"))
+        if (albums.isNotEmpty()) {
+            mBinding!!.rvAlbumList.visibility = View.VISIBLE
+            mBinding!!.tvAlbumAvailable.visibility = View.GONE
+            adapter.albumList(albums)
+        } else {
+            mBinding!!.rvAlbumList.visibility = View.GONE
+            mBinding!!.tvAlbumAvailable.visibility = View.VISIBLE
+        }
+    }
+
 }
